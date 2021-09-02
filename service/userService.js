3a8908a7069447c6c83f0fec9f0e037d79cca4af
saikumar2912 <ksai4666@gmail.com>
2021-09-02 13:03:32 +0530

    migrated to couchdb

---
+++ service/userService.js
@@ -0,0 +1,52 @@
+class UserService{
+    static register(data){
+        const url = "https://libraryapp-node-api.herokuapp.com/users/addUser";
+return axios.post(url,data);
+    }
+    static login(data){
+        const url ='https://libraryapp-node-api.herokuapp.com/users/login';
+        return axios.post(url,data);
+    }
+    static allBooks(){
+        const url ='https://libraryapp-node-api.herokuapp.com/book/get-all-books?status=Active';
+        return axios.get(url)
+    }
+
+    static loadBook(id){
+        const url =('https://libraryapp-node-api.herokuapp.com/book/get-book-by-id/' + id);
+        return axios.get(url);
+    }
+    static orderBook(Obj){
+        const url='https://libraryapp-node-api.herokuapp.com/order/place-orders/'
+        return axios.post(url,Obj)
+    }
+    static orderDetails(userId){
+        const url=('https://libraryapp-node-api.herokuapp.com/order/order-details/'+userId)
+        return axios.get(url);
+    }
+    static returnBook(bid,uid){
+        const url=`https://libraryapp-node-api.herokuapp.com/order/return-book/${uid}/${bid}`
+        return axios.patch(url)
+    }
+    static renewBook(bid,uid){
+        const url=`https://libraryapp-node-api.herokuapp.com/order/renew-date/${uid}/${bid}`
+        return axios.patch(url)
+    }
+    static addRequest(obj){
+        const url='https://libraryapp-node-api.herokuapp.com/request/add-request'
+        return axios.post(url,obj)
+    }
+    static getRequestedBooks(){
+    const url='https://libraryapp-node-api.herokuapp.com/request/all-requests'
+return axios.get(url)
+    }
+    static updateBookCount(Obj){
+        const url=`https://libraryapp-node-api.herokuapp.com/request/add-count`
+    return axios.post(url,Obj)
+        }
+    static userDetails(){
+        const userStr = localStorage.getItem("user");
+        return JSON.parse(userStr);
+    
+    }
+}
